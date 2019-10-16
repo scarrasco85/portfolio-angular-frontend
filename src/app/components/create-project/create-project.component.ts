3a290c009376f78b775366cf47ea773df03c80b1
Sergio85 <chechucarrasco@gmail.com>
2019-10-16 20:50:55 +0200

    Empezamos a trabajar sobre el componente CreateProject y creamos el formulario

--- src/app/components/create-project/create-project.component.ts
+++ src/app/components/create-project/create-project.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+//Modelo del componente
+import { Project } from '../../models/project';
+//Servicio del componente
+import { ProjectService } from '../../services/project.service';
 
 @Component({
   selector: 'app-create-project',
   templateUrl: './create-project.component.html',
-  styleUrls: ['./create-project.component.css']
+  styleUrls: ['./create-project.component.css'],
+  //Cargamos servicio del componente
+  providers: [ProjectService]
 })
 export class CreateProjectComponent implements OnInit {
 
-  constructor() { }
+	public title: string;
+	//Propiedad de tipo Project para el nuevo proyecto
+	public project: Project;
+	
+	constructor(
+		//Creamos las propiedades del Servicio
+		private _projectService: ProjectService
+	){
+		//Inicializamos las propiedades
+		this.title = "Crear nuevo proyecto";
+		//Creamos una instancia de objeto Project (modelo componente)
+		this.project = new Project('','','','',2019,'','');
+	 }
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
 
 }
