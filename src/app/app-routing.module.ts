c729701ea38df6e0ff4b2ad5317e66d1f07bdd68
Sergio85 <chechucarrasco@gmail.com>
2019-10-16 11:27:09 +0200

    Añadimos configuración ruta para el componente error en el archivo de configuración de rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,13 +6,15 @@ import { AboutComponent } from './components/about/about.component';
 import { ProjectsComponent } from './components/projects/projects.component';
 import { CreateProjectComponent } from './components/create-project/create-project.component';
 import { ContactComponent } from './components/contact/contact.component';
+import { ErrorComponent } from './components/error/error.component';
 
 const routes: Routes = [
 	{path: '', component: AboutComponent},
 	{path: 'sobre-mi', component: AboutComponent},
 	{path: 'proyectos', component: ProjectsComponent},
 	{path: 'crear-proyecto', component: CreateProjectComponent},
-	{path: 'contacto', component: ContactComponent}
+	{path: 'contacto', component: ContactComponent},
+	{path: '**', component: ErrorComponent}
 ];
 
 @NgModule({
