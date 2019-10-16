05a90aa568d61032ceca57921bb78e3501d38ce1
Sergio85 <chechucarrasco@gmail.com>
2019-10-16 11:20:35 +0200

    Añadimos configuración de rutas de los componentes creados en el archivo de configuración de rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+//Importamos nuestros componentes
+import { AboutComponent } from './components/about/about.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { CreateProjectComponent } from './components/create-project/create-project.component';
+import { ContactComponent } from './components/contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{path: '', component: AboutComponent},
+	{path: 'sobre-mi', component: AboutComponent},
+	{path: 'proyectos', component: ProjectsComponent},
+	{path: 'crear-proyecto', component: CreateProjectComponent},
+	{path: 'contacto', component: ContactComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
