47267335278938ab033e88b566ea0ab6075b4296
Sergio <chechucarrasco@gmail.com>
2020-11-04 16:12:10 +0100

    Modificación ruta backend

--- src/app/services/global.ts
+++ src/app/services/global.ts
@@ -1,5 +1,6 @@
 // ** Archivo creado para configuraciones globales
 export var Global = {
 	//url Api-Rest
-	url: 'http://localhost:3700/api/'
+	//url: 'http://localhost:3700/api/'
+	url: 'https://portafolio-angular-nodejs.herokuapp.com/api/'
 };
