ccd3587a40110a6f1576e0fb1fd5cd2cde1ba274
Sergio <chechucarrasco@gmail.com>
2020-11-04 17:24:45 +0100

    remove files

--- src/app/services/global.ts
+++
@@ -1,6 +0,0 @@
-// ** Archivo creado para configuraciones globales
-export var Global = {
-	//url Api-Rest
-	//url: 'http://localhost:3700/api/'
-	url: 'https://portafolio-angular-nodejs.herokuapp.com/api/'
-};
